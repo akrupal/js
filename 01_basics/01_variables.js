37a79b2d3da2d481d89135b0e6f7414b08fb6998
akrupal <162036218+akrupal@users.noreply.github.com>
2024-03-03 17:24:00 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "abc@google.com"
+var accountPassword = "12345"
+accountCity = "Nagpur"
+
+//accountId = 2 // not allowed
+accountEmail = "abc@gmail.com"
+accountPassword = "23456"
+accountCity = "NGP"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; // treats all JS code as newer version
+//alert(3+3) // will work in browser but not here
+
+// number => 2 to pow 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (type is object)
+// undefined
+// symbol => uniqueness
+// object
+
+console.log(typeof 2);
